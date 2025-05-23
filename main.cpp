8b3645c0c38632dc0545985b66f6c7b530607fd3
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2025-05-22 21:12:50 -0300

    Ball now gradually speeds up

--- main.cpp
+++ main.cpp
@@ -1,75 +1,86 @@
 #include "raylib.h"
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Pong com Bot - raylib");
+  InitWindow(screenWidth, screenHeight, "Pong raylib");
 
-    Rectangle player = { 50, screenHeight / 2 - 40, 20, 80 };
+  Rectangle player = {50, screenHeight / 2 - 40, 20, 80};
 
-    Rectangle bot = { screenWidth - 70, screenHeight / 2 - 40, 20, 80 };
+  Rectangle bot = {screenWidth - 70, screenHeight / 2 - 40, 20, 80};
 
-    Vector2 ballPosition = { screenWidth / 2, screenHeight / 2 };
-    Vector2 ballSpeed = { 4.0f, 4.0f };
-    float ballRadius = 10;
+  Vector2 ballPosition = {screenWidth / 2, screenHeight / 2};
+  Vector2 ballSpeed = {4.0f, 4.0f};
+  float ballRadius = 10;
 
-    int playerScore = 0;
-    int botScore = 0;
+  int playerScore = 0;
+  int botScore = 0;
 
-    SetTargetFPS(60);
+  SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_W)) player.y -= 5.0f;
-        if (IsKeyDown(KEY_S)) player.y += 5.0f;
+  while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_W))
+      player.y -= 5.0f;
+    if (IsKeyDown(KEY_S))
+      player.y += 5.0f;
 
-        if (player.y < 0) player.y = 0;
-        if (player.y + player.height > screenHeight) player.y = screenHeight - player.height;
+    if (player.y < 0)
+      player.y = 0;
+    if (player.y + player.height > screenHeight)
+      player.y = screenHeight - player.height;
 
-        if (ballPosition.y < bot.y + bot.height / 2) bot.y -= 4.0f;
-        if (ballPosition.y > bot.y + bot.height / 2) bot.y += 4.0f;
+    if (ballPosition.y < bot.y + bot.height / 2)
+      bot.y -= 4.0f;
+    if (ballPosition.y > bot.y + bot.height / 2)
+      bot.y += 4.0f;
 
-        if (bot.y < 0) bot.y = 0;
-        if (bot.y + bot.height > screenHeight) bot.y = screenHeight - bot.height;
+    if (bot.y < 0)
+      bot.y = 0;
+    if (bot.y + bot.height > screenHeight)
+      bot.y = screenHeight - bot.height;
 
-        ballPosition.x += ballSpeed.x;
-        ballPosition.y += ballSpeed.y;
+    ballPosition.x += ballSpeed.x;
+    ballPosition.y += ballSpeed.y;
 
-        if (ballPosition.y - ballRadius <= 0 || ballPosition.y + ballRadius >= screenHeight)
-            ballSpeed.y *= -1;
+    if (ballPosition.y - ballRadius <= 0 ||
+        ballPosition.y + ballRadius >= screenHeight)
+      ballSpeed.y *= -1;
 
-        if (CheckCollisionCircleRec(ballPosition, ballRadius, player) ||
-            CheckCollisionCircleRec(ballPosition, ballRadius, bot)) {
-            ballSpeed.x *= -1;
-        }
-
-        if (ballPosition.x - ballRadius < 0) {
-            botScore++;
-            ballPosition = { screenWidth / 2, screenHeight / 2 };
-            ballSpeed = { 4.0f, 4.0f };
-        }
-
-        if (ballPosition.x + ballRadius > screenWidth) {
-            playerScore++;
-            ballPosition = { screenWidth / 2, screenHeight / 2 };
-            ballSpeed = { -4.0f, 4.0f };
-        }
-
-        BeginDrawing();
-        ClearBackground(DARKGRAY);
-
-        DrawText("PONG com Bot - raylib", screenWidth/2 - 100, 10, 20, LIGHTGRAY);
-
-        DrawText(TextFormat("Jogador: %i", playerScore), 10, 40, 20, WHITE);
-        DrawText(TextFormat("Bot: %i", botScore), screenWidth - 150, 40, 20, WHITE);
-
-        DrawRectangleRec(player, WHITE);
-        DrawRectangleRec(bot, WHITE);
-        DrawCircleV(ballPosition, ballRadius, RED);
-
-        EndDrawing();
+    if (CheckCollisionCircleRec(ballPosition, ballRadius, player) ||
+        CheckCollisionCircleRec(ballPosition, ballRadius, bot)) {
+      ballSpeed.x *= -1;
+      ballSpeed.x *= 1.05f;
+      ballSpeed.y *= 1.05f;
     }
 
-    CloseWindow();
-    return 0;
+    if (ballPosition.x - ballRadius < 0) {
+      botScore++;
+      ballPosition = {screenWidth / 2, screenHeight / 2};
+      ballSpeed = {4.0f, 4.0f};
+    }
+
+    if (ballPosition.x + ballRadius > screenWidth) {
+      playerScore++;
+      ballPosition = {screenWidth / 2, screenHeight / 2};
+      ballSpeed = {-4.0f, 4.0f};
+    }
+
+    BeginDrawing();
+    ClearBackground(DARKGRAY);
+
+    DrawText("PONG raylib", screenWidth / 2 - 100, 10, 20, LIGHTGRAY);
+
+    DrawText(TextFormat("Jogador: %i", playerScore), 10, 40, 20, WHITE);
+    DrawText(TextFormat("Bot: %i", botScore), screenWidth - 150, 40, 20, WHITE);
+
+    DrawRectangleRec(player, WHITE);
+    DrawRectangleRec(bot, WHITE);
+    DrawCircleV(ballPosition, ballRadius, RED);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
